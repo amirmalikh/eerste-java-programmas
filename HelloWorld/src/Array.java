e745e489626b3594c1604c2800716ee8578aa87a
Amir Malik <amir-malik-h@hotmail.com>
2018-06-01 13:08:25 +0200

    middag

---
+++ HelloWorld/src/Array.java
@@ -0,0 +1,76 @@
+public class Array {
+    public static void main(String[] args) {
+     /*   int b = 5; // scalar, variable
+
+        int[] j = {1, 2, 5, 7, 9, -6, 12};
+        System.out.println(j.length);
+
+        int index = 0; // berekend
+        int getal = j[index];
+        System.out.println(getal);
+
+
+        for (int i = 0; i < j.length; i++) {
+            System.out.println(j[i]);
+            }
+
+    */
+
+
+   /*     String[] names = {"Jimmy", "Janice", "Jim", "Amy", "Kurt"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+*/
+ /*       String[] mailLijst = {
+                "Jimmy@gmail.com",
+                "Janice@gmail.com",
+                "Jim@gmail.com",
+                "Amy@gmail.com",
+                "Kurt@gmail.com"
+        };
+
+
+        for (int i = 0; i < mailLijst.length; i++) {
+            System.out.println(mailLijst[i]);
+        }
+
+        for (String naam : mailLijst){
+            stuuEmailNaar(naam);
+        }
+*/
+
+/*
+        int[] j = {1, 2, 5, 7, 9, -6, 12};
+
+
+        for (int i = 0; i < j.length; i++) {
+
+            System.out.println(j[i] *= 2);
+        }
+*/
+
+/*
+
+        int[] getallen = new int[20];     //een array met 20 beschikbare velden
+
+        for (int i : getallen) {
+            System.out.println(i);
+        }
+*/
+
+        String s = null;
+        System.out.println("ERVOOR");
+
+        if (s != null) {
+            s.toUpperCase();
+        }
+        else {
+            System.out.println("Uppercase niet mogelijk");
+        }
+
+    }
+
+}
+
---
+++ HelloWorld/src/TaxCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+
+
+        System.out.print("Please enter gross monthly income (in euro): ");
+      //  Scanner inputLoon = new Scanner(System.in);
+      //  double loonInput = inputLoon.nextDouble();
+        double grossIncome = 10000;
+        System.out.println(grossIncome);
+        double rszWaarde = .1307;
+        double rsz  = grossIncome* rszWaarde;
+        System.out.println("Social security (13.07%): " + rsz);
+
+        double taxableIncome = grossIncome- rsz;
+        System.out.println("Taxable income: " + taxableIncome);
+
+        double[] incomeList = {0, 8680, 12360, 20600, 37750};
+        double[] taxationAmount = {25, 30, 40, 45, 50};
+
+        
+
+
+        double totalTaxValue = 0;
+
+
+
+
+
+    }
+
+
+
+
+}
